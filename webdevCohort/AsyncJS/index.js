8f7e65ca9eb6d86080c24f5d083724542ecec068
Harsh <harshradadiya9999@gmail.com>
2024-09-25 12:11:16 +0530

    updare

--- webdevCohort/AsyncJS/index.js
+++ webdevCohort/AsyncJS/index.js
@@ -1,7 +1,25 @@
+const { log } = require("console");
 const fs = require("fs");
 
-const data1 = fs.readFileSync("b.txt", "utf8");
-console.log(data1);
-const data = fs.readFileSync("a.txt", "utf8");
-console.log(data);
+// const data1 = fs.readFileSync("b.txt", "utf8");
+// console.log(data1);
+// const data = fs.readFileSync("a.txt", "utf8");
+// console.log(data);
 
+
+const printData = (data) => {
+  console.log(data);
+};
+
+fs.readFile("a.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  printData(data);
+});
+
+fs.readFile("b.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  printData(data);
+});
+
+
+log("Hello from from index.js");
